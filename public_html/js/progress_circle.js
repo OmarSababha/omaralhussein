aff94d63ee5c57235dbee7b49f2449aacece93f7
Omar Alhussein <OmarSababha@users.noreply.github.com>
2021-09-17 20:18:08 -0400

    Delete public_html directory
    delete public_html; redundant.

--- public_html/js/progress_circle.js
+++
@@ -1,9 +0,0 @@
-$('head style[type="text/css"]').attr('type', 'text/less');
-less.refreshStyles();
-window.randomize = function() {
-	$('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
-}
-setTimeout(window.randomize, 200);
-$('.radial-progress').click(window.randomize);
-
-// Read more here: https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
